34c981df795ac6bee5f78d894c9d680302e43573
Konstantin <78598480+KonstantinnVolkov@users.noreply.github.com>
2022-06-25 21:23:42 +0300

    Delete src/main directory

--- src/main/kotlin/models/figures/Circle.kt
+++
@@ -1,9 +0,0 @@
-package models.figures
-
-import models.abstraction.SymShape
-
-data class Circle (
-    override var x: Double,
-    override var y: Double,
-    var side: Double
-): SymShape(x, y, side)
